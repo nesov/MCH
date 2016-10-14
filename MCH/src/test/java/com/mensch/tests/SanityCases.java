64cf69fd0fb67e128181fd3e802e1f6fd7f9beef
Alex Nesov <magicofasword@gmail.com>
2016-10-15 00:23:01 +0300

    no message

--- MCH/src/test/java/com/mensch/tests/SanityCases.java
+++ MCH/src/test/java/com/mensch/tests/SanityCases.java
@@ -1,81 +1,75 @@
 
-package com.mensch.tests;
+ package com.mensch.tests;
 
+ import mensch.pages.LoginPage;
+ import mensch.pages.ForgotPasswordPage;
 
  import org.junit.After;
  import org.junit.Before;
+ import org.junit.FixMethodOrder;
  import org.junit.Test;
- import org.openqa.selenium.*;
+ import org.junit.runners.MethodSorters;
+ import org.openqa.selenium.Dimension;
+ import org.openqa.selenium.WebDriver;
  import org.openqa.selenium.chrome.ChromeDriver;
+ import mensch.pages.constants.INSTANCE;
+ import org.openqa.selenium.support.ThreadGuard;
 
- import static org.openqa.selenium.By.id;
-
-
-public class SanityCases {
-
-
-    WebDriver driver = new ChromeDriver();
+ import java.util.concurrent.TimeUnit;
 
 
-    private static String MENSCH_URL = "http://dev-mensch.synergetica.net/login";
-    private static String EMAIL_INPUT = "email";
-    private static String PASSWORD_INPUT = "password";
-    private static String FORGOT_PASSWORD_LINK = "//div/div/div[2]/form/a/span";
-
-    public String generateLogin(){
-        String login = "a.nesov@synergetica.co";
-        return login;
-    }
-    public String generatePassword (){
-        String password = "Admin321";
-        return password;
-    }
-
-
-
-
+ @FixMethodOrder (MethodSorters.NAME_ASCENDING)
+ public class SanityCases {
+     WebDriver driver;
+     private String prep;
 
     @Before
-    public void startDriver() {
+    public void setup(){
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+
+        Dimension d = new Dimension(800,600);
+        prep = System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = ThreadGuard.protect(new ChromeDriver());
+        driver.manage().window().setSize(d);
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 
     }
 
     @After
-    public void stopDriver(){
+    public void teardown(){
         driver.quit();
-        driver.close();
-    }
-
-
-
-    @Test
-    public void checkForgotPassword(){
-        System.setProperty("webdriver.chrome.driver", "/drivers/chromedriver");
-
-        driver.get(MENSCH_URL);
-        WebElement forgotPasswordLink = driver.findElement(By.xpath(FORGOT_PASSWORD_LINK));
-        forgotPasswordLink.click();
-
-
     }
 
     @Test
-    public void loginCase() throws Exception{
+    public void AcheckForgotPassword() throws Exception {
+        LoginPage loginPage = new LoginPage(driver);
+        ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage(driver);
+        loginPage.openLoginPage(driver,INSTANCE.DEV);
+        loginPage.clickForgotPasswordLink();
+        forgotPasswordPage.fillOutEmailField("owner@teleworm.us");
+        forgotPasswordPage.clickSendButton();
+        forgotPasswordPage.getBackToLoginPage(driver);
+    }
 
-        driver.get(MENSCH_URL);
+    @Test
+    public void BloginCase() throws Exception{
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openLoginPage(driver,INSTANCE.DEV);
+        loginPage.fillOutEmail("owner@teleworm.us");
+        loginPage.fillOutPassword("Admin321");
+        loginPage.clickSendButton();
+        loginPage.clickSendButton();
+    }
 
-        driver.findElement(By.name(EMAIL_INPUT)).clear();
-        driver.findElement(By.name(EMAIL_INPUT)).sendKeys(generateLogin());
-
-        driver.findElement(By.name(PASSWORD_INPUT)).clear();
-        driver.findElement(By.name(PASSWORD_INPUT)).sendKeys(generatePassword());
-
-        WebElement loginButton = driver.findElement(id("loginBtn"));
-
-        for (int i = 0; i < 2; i++){
-           Thread.sleep(5000);
-           loginButton.click();
-       }
-        Thread.sleep(10000);
+    @Test
+     public void CopenForgotPasswordScreen()throws Exception{
+        LoginPage loginPage = new LoginPage(driver);
+        ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage(driver);
+        loginPage.openLoginPage(driver,INSTANCE.DEV);
+        loginPage.clickToForgotPasswordLinkByMouse(driver);
+        forgotPasswordPage.fillOutEmailField("owner@cuvox.de");
+        forgotPasswordPage.clickSendButton();
+        forgotPasswordPage.getBackToLoginPage(driver);
     }
 }
